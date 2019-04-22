1d52d5068d2109a667f6475ea7f1711aeb797217
Fredy Abel Huanca Torres <abel.huanca@upeu.edu.pe>
2019-04-21 20:48:15 -0500

    inicializando proyecto

---
+++ services/users/project/__init__.py
@@ -0,0 +1,16 @@
+# services/users/project/__init__.py
+
+
+from flask import Flask, jsonify
+
+
+# instanciando app
+app = Flask(__name__)
+
+
+@app.route('/users/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })
